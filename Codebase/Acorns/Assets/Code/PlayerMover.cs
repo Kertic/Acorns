acca799f7431b990f1944effefa36a8632094859
Kertic <yougotbrennan@gmail.com>
2019-12-20 18:03:28 -0600

    Organized code into folder

---
+++ Codebase/Acorns/Assets/Code/PlayerMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        private Rigidbody k_rigidBody;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+            k_rigidBody = GetComponent<Rigidbody>();
+        
+            k_rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+                k_rigidBody.velocity += transform.forward;
+            if (Input.GetKey(KeyCode.S))
+                k_rigidBody.velocity -= transform.forward;
+
+            if (Input.GetKey(KeyCode.A))
+                k_rigidBody.velocity -= transform.right;
+            if (Input.GetKey(KeyCode.D))
+                k_rigidBody.velocity += transform.right;
+        }
+    }
+}
